8e48678ee7d5283feaafefae243369cf266e3374
kamran1007 <82281265+kamran1007@users.noreply.github.com>
2023-01-12 20:22:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,9 +17,10 @@ app.use("/user", userRouter);
 
 // mongodb connection
 const PORT = process.env.PORT
- 
+const URI = process.env.CONNECTION_URL;
+
 mongoose
-  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
     app.listen(PORT, () => console.log(`server is running on port: http://localhost:${PORT}`))
   )
